1642721706ef3fb66336eac2cf239c9cfa5192e4
YiChiaTsai <richard.yctsai@gmail.com>
2017-01-02 01:54:29 +0800

    Complete plotting program and README.md.

---
+++ Acc_gyro/MPU6050/MPU6050.ino
@@ -0,0 +1,131 @@
+// I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050 class
+// 10/7/2011 by Jeff Rowberg <jeff@rowberg.net>
+// Updates should (hopefully) always be available at https://github.com/jrowberg/i2cdevlib
+//
+// Changelog:
+//     2011-10-07 - initial release
+
+/* ============================================
+I2Cdev device library code is placed under the MIT license
+Copyright (c) 2011 Jeff Rowberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+===============================================
+*/
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#include "Wire.h"
+
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+float accelNorm = 0;
+
+#define LED_PIN 13
+bool blinkState = false;
+int showinterval = 0;
+
+const int numReadings = 10;
+float readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+float total = 0;                  // the running total
+float average = 0;                // the average
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    Wire.begin();
+
+    // initialize serial communication
+    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+    // it's really up to you depending on your project)
+    Serial.begin(9600);
+
+    // initialize device
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+    // configure Arduino LED for
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    // read raw accel/gyro measurements from device
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
+
+    accelNorm = mySmoothing(sqrt(pow(ax, 2) + pow(ay, 2) + pow(az, 2)));
+//     display tab-separated accel/gyro x/y/z values
+      Serial.println(accelNorm);
+      Serial.print("a/g:\t");
+      Serial.print(ax); Serial.print("\t");
+      Serial.print(ay); Serial.print("\t");
+      Serial.print(az); Serial.print("\t");
+      Serial.print(gx); Serial.print("\t");
+      Serial.print(gy); Serial.print("\t");
+      Serial.println(gz);
+      delay(100);
+    
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+}
+
+int mySmoothing(int fsensorValue){
+  
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  // readings[readIndex] = analogRead(inputPin);
+  readings[readIndex] = fsensorValue;
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+  //  Serial.println(average);
+  delay(1);        // delay in between reads for stability
+  return average;
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # MPU6050-and-HMC5883L
 Acceleration, Gyroscope and Magnetic
+
+Motion detection using MPU 6050
+– Use MPU 6050 to detect the vibration of our
+hands, and show events on serial terminal.
+– For example, you can tie it on your wrist, then
+detect the events:
+• when you are typing a keyboard,
+• when you are shaking,
+• when you are using a smartphone touchscreen, • when you are writing on a piece of paper, etc.
+
+In the battle, sometimes it is difficult to estimate the distance of two objects in enemy's field. Please design a distance estimator for this purpose. As shown in the Figure below, we want to know the distance between A and B. Please use ultrasonic sensor to measure the distances of AC and BC, and use HMC 5883L to estimate the angle of ∠ACB.
+
+Steps (you are in point C):
+1. Press button to measure the distance of AC
+2. Rotate from pointing to A to pointing to B
+3. Press button to measure the distance of BC
+4. Print out the distance between A and B
+Hint: Law of cosines.
